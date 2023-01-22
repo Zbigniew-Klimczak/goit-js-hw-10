01c8e0cbc6099e15d87636bffe46364f6276a8e5
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-01-22 22:50:50 +0100

    Started

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
-
+import _ from 'lodash';
+import Notiflix from 'notiflix';
+import fetchCountries from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+const countryInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+countryInput.addEventListener(
+  'input',
+  _.debounce(evt => {
+    let countryName = evt.target.value.trim();
+    if (countryName !== '') {
+      console.log(countryName);
+    }
+  }, DEBOUNCE_DELAY)
+);
